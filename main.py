563fe8e4fb26852d4e69fa46d1ffb6b26c111a6b
John Simonsen <jsimo60@wgu.edu>
2026-04-02 12:19:10 -0600

    Got the RSS feed urls.

---
+++ main.py
@@ -0,0 +1,9 @@
+FEEDS={
+	'IBM Technology': "https://youtube.com/feeds/videos.xml?channel_id=UCKWaEZ-_VweaEx1j62do_vQ",
+	'The AI Daily Brief': "https://anchor.fm/s/f7cac464/podcast/rss",
+	'The AI in Business Podcast': "https://techemergence.libsyn.com/rss",
+	'Practical AI': "https://feeds.transistor.fm/practical-ai-machine-learning-data-science-llm"
+}
+
+
+
